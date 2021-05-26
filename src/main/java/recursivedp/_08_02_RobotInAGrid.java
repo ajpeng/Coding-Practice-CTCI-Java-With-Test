72a71110df70dc950f3834541274586c19f369d6
unknown <alexpeng014@gmail.com>
2021-05-26 12:53:17 -0400

    Updated files for personal solution

--- src/main/java/recursivedp/_08_02_RobotInAGrid.java
+++ src/main/java/recursivedp/_08_02_RobotInAGrid.java
@@ -1,7 +1,6 @@
 package recursivedp;
 
 import java.awt.*;
-import java.util.*;
 import java.util.List;
 
 /**
@@ -12,31 +11,8 @@ import java.util.List;
  */
 class _08_02_RobotInAGrid {
     List<Point> findPath(boolean[][] grid) {
-        if (grid == null || grid.length == 0) return Collections.emptyList();
-        int rows = grid.length - 1;
-        int cols = grid[0].length - 1;
-        List<Point> path = new ArrayList<>();
-        Set<Point> failed = new HashSet<>();
-        getPath(grid, rows, cols, path, failed);
-        return path;
+        throw new UnsupportedOperationException();
     }
 
-    private boolean getPath(boolean[][] grid, int r, int c, List<Point> path, Set<Point> failed) {
-        if (r < 0 || c < 0 || !grid[r][c]) {
-            return false;
-        }
-        Point p = new Point(r, c);
-
-        if (failed.contains(p)) return false;
-
-        boolean isOrigin = (r == 0) && (c == 0);
-        if (isOrigin || getPath(grid, r - 1, c, path, failed) || getPath(grid, r, c - 1, path, failed)) {
-            path.add(p);
-            return true;
-        } else {
-            failed.add(p);
-            return false;
-        }
-    }
 
 }
