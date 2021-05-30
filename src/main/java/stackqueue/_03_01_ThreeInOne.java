c8b6e904d57e188f50c601ad7944b60d914aa1cf
unknown <alexpeng014@gmail.com>
2021-05-30 15:51:43 -0400

    min stack solution

--- src/main/java/stackqueue/_03_01_ThreeInOne.java
+++ src/main/java/stackqueue/_03_01_ThreeInOne.java
@@ -1,21 +1,56 @@
 package stackqueue;
 
+import java.util.Arrays;
+
 /**
  * Describe how you could use a single array to implement three stacks
  */
 class _03_01_ThreeInOne {
-
+    int[] values;
+    int[] stackSize;
+    int capacity;
+    int numberOfStacks = 3;
 
     _03_01_ThreeInOne(int initSize) {
-
+        values = new int[initSize * numberOfStacks];
+        stackSize = new int[numberOfStacks];
+        capacity = initSize;
     }
 
-    void push(int stackNum, int val) {
-        throw new UnsupportedOperationException();
+    void push(int stackNum, int val) throws UnsupportedOperationException{
+
+        checkIfFull(stackNum);
+        stackSize[stackNum]++;
+        values[indexOfTop(stackNum)] = val;
+        System.out.println(Arrays.toString(values));
     }
 
-    int pop(int stackNum) {
-        throw new UnsupportedOperationException();
+    int pop(int stackNum) throws UnsupportedOperationException {
+        // if there are still elements pop
+        if (!isEmpty(stackNum)) {
+            int res = values[indexOfTop(stackNum)];
+            values[indexOfTop(stackNum)] = 0;
+            stackSize[stackNum]--;
+            return res;
+        } else {
+            throw new UnsupportedOperationException("Cannot pop empty stack");
+        }
+    }
+
+    boolean isEmpty(int stackNum){
+        return stackSize[stackNum] == 0;
+    }
+
+    void checkIfFull(int stackNum) {
+        if (stackSize[stackNum] == capacity) {
+            throw new UnsupportedOperationException("This stack is already full");
+        }
+    }
+
+    private int indexOfTop(int stackNum) {
+        int offset = stackNum * capacity;
+        int size = stackSize[stackNum];
+        return offset + size - 1;
     }
 
 
--- src/main/java/stackqueue/_03_02_MinStack.java
+++ src/main/java/stackqueue/_03_02_MinStack.java
@@ -1,20 +1,30 @@
 package stackqueue;
 
+import java.util.Stack;
+
 /**
  * How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? Push, pop and min should all operate in 0(1) time.
  */
 class _03_02_MinStack {
-
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack();
 
     void push(int val) {
-        throw new UnsupportedOperationException();
+        // checking for less than or equal for edge cases for min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.add(val);
+        }
+        stack.push(val);
     }
 
     int pop() {
-        throw new UnsupportedOperationException();
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        return stack.pop();
     }
 
     int min() {
-        throw new UnsupportedOperationException();
+        return minStack.peek();
     }
 }
